0a724b2e08abade949ca7718d24f46c3148eb963
zhy5571 <zhy5571ff@163.com>
2023-06-10 22:22:25 +0800

    initialize project architecture

---
+++ gulimall-coupon/src/test/java/com/zhy/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhy.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
