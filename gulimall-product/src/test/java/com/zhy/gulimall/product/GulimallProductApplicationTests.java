cf5e66fdb4ba2f2dea69e359b63871f930af0496
zhy5571 <zhy5571ff@163.com>
2023-06-12 11:33:18 +0800

    initialize project architecture

--- gulimall-product/src/test/java/com/zhy/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/zhy/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,30 @@
 package com.zhy.gulimall.product;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.zhy.gulimall.product.entity.BrandEntity;
+import com.zhy.gulimall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
 
+    @Autowired
+    BrandService brandService;
     @Test
     void contextLoads() {
+
+//        BrandEntity brandEntity = new BrandEntity();
+//        brandEntity.setName("huawei");
+//        brandService.save(brandEntity);
+//        System.out.println("保存成功。。。");b
+        List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 1L));
+        for (BrandEntity item : list) {
+            System.out.println(item);
+        }
     }
 
 }
